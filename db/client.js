00675ee9088a56a3cb96dc7fd852b7ea617b33e6
Ian Lee <inpyoianlee@gmail.com>
2022-09-07 20:54:39 -0700

    getting backend started

---
+++ db/client.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const { Client } = require('pg');
+const DB_NAME = 'todoapp'
+const DB_URL = process.env.DATABASE_URL || `postgres://postgres@localhost:5432/${ DB_NAME }`;
+// const CONNECTION_STRING = process.env.DATABASE_URL || `postgres://postgres@localhost:5432/fitness-dev`
+const client = new Client(DB_URL);
+
+module.exports = client;
